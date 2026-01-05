c104071fffc1688ce5df7760501ce4499165a562
BilalGULDEN80 <bilalgulden@gmail.com>
2026-01-06 00:22:11 +0300

    Duzeltme

--- Odev-1/main.c
+++ Odev-1/main.c
@@ -1,15 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	int[][] sparsematrix;
-	pirntf("satir,sutun,deger girin");
-	
-	return 0;
-}
-int [][] SparseMatrixVeriEkle(int[][] Array3d,int col,int satir,int deger)
+int main(int argc, char *argv[]) 
 {
-	
+    int _xiKullaniciSecim;
+    printf("Kendin Deger Girerek mi Olusturacaksan 1, hazir matrix kullanacaksan 2 Gir: ");
+    scanf("%d", &_xiKullaniciSecim);	
+	/* yapay zeka deðil El Emeðidir Hocam */
+    int satir, sutun;
+    int sparseMatrix[100][100]; 
+    int i, j;
+
+    if (_xiKullaniciSecim == 1) {
+        printf("Satir Sayisini Girin: ");
+        scanf("%d", &satir);
+        printf("Sutun Sayisini Girin: ");
+        scanf("%d", &sutun);
+
+        printf("Matris elemanlarini girin (%dx%d):\n", satir, sutun);
+        for (i = 0; i < satir; i++) {
+            for (j = 0; j < sutun; j++) {
+                printf("matris[%d][%d] = ", i, j);
+                scanf("%d", &sparseMatrix[i][j]);
+            }
+        }
+    }
+    else if (_xiKullaniciSecim == 2) 
+	{
+        //Kaynak Olarak Paylaþtýðýnýz Book1.psf'deki hazýr örnek matris
+        satir = 4;
+        sutun = 5;
+        int temp[4][5] = {
+            {0, 0, 3, 0, 4},
+            {0, 0, 5, 7, 0},
+            {0, 0, 0, 0, 0},
+            {0, 2, 6, 0, 0}
+        };       
+        for (i = 0; i < satir; i++) {
+            for (j = 0; j < sutun; j++) {
+                sparseMatrix[i][j] = temp[i][j];
+            }
+        }
+    }
+    else {
+        printf("Gecersiz secim!\n");
+        return 1;
+    }    
+    printf("\nOrijinal Matris (%dx%d):\n", satir, sutun);
+    for (i = 0; i < satir; i++) {
+        for (j = 0; j < sutun; j++) {
+            printf("%4d ", sparseMatrix[i][j]);
+        }
+        printf("\n");
+    }    
+    int sifirOlmayanSayisi = 0;
+    for (i = 0; i < satir; i++) {
+        for (j = 0; j < sutun; j++) {
+            if (sparseMatrix[i][j] != 0) {
+                sifirOlmayanSayisi++;
+            }
+        }
+    }   
+    int compressed[sifirOlmayanSayisi + 1][3];
+
+    compressed[0][0] = satir;
+    compressed[0][1] = sutun;
+    compressed[0][2] = sifirOlmayanSayisi;
+
+    int k = 1; 
+    for (i = 0; i < satir; i++) {
+        for (j = 0; j < sutun; j++) {
+            if (sparseMatrix[i][j] != 0) {
+                compressed[k][0] = i;      // satýr 
+                compressed[k][1] = j;      // sütun 
+                compressed[k][2] = sparseMatrix[i][j];
+                k++;
+            }
+        }
+    }   
+    printf("\nSikistirilmis Sparse Matris:\n");
+    printf("Satir\tSutun\tDeger\n");
+    for (i = 0; i <= sifirOlmayanSayisi; i++) {
+        printf("%d\t%d\t%d\n", compressed[i][0], compressed[i][1], compressed[i][2]);
+    }
+
+    return 0;
 }
--- Odev-2/main.c
+++ Odev-2/main.c
@@ -1,8 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#define MAX 100
+char stack[MAX];
+int top = -1;
 
-int main(int argc, char *argv[]) {
-	return 0;
+void push(char Girdi) {
+    if (top >= MAX - 1) {
+        printf("Stack Dolu\n");
+    } else {
+        stack[++top] = Girdi;
+    }
+}
+char pop() {
+    if (top == -1) {
+        return -1;
+    } else {
+        return stack[top--];
+    }
+}
+char peek() {
+    if (top == -1) {
+        return -1;
+    }
+    return stack[top];
+}
+int oncelik(char ch) {
+    if (ch == '+' || ch == '-')
+        return 1;
+    if (ch == '*' || ch == '/')
+        return 2;
+    if (ch == '^') 
+        return 3;
+    return 0;
+}
+int OperatorMu(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^');
+}
+void infixPostfixeCevir(char* infix, char* postfix) {
+    int i, j = 0;
+    char _xValue;
+    char x;
+    for (i = 0; infix[i] != '\0'; i++) {
+        _xValue = infix[i];       
+        if (isalnum(_xValue)) {//Hocam isalnum ifadesi sayý mý karektermi harfmi ona bakýyor
+            postfix[j++] = _xValue;
+        }        
+        else if (_xValue == '(') {
+            push(_xValue);
+        }     
+        else if (_xValue == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); 
+        }        
+        else if (OperatorMu(_xValue)) {
+            // Stack boþ deðilse ve stack'teki operatörün önceliði >= ise pop yap
+            while (top != -1 && oncelik(peek()) >= oncelik(_xValue)) {
+                postfix[j++] = pop();
+            }
+            push(_xValue);
+        }
+    }  
+    postfix[j] = '\0'; 
+}
+
+int main() {
+    
+    char infix[MAX] = "A+b*(c^d-e)/(F+g*h)-i";
+    char postfix[MAX];
+
+    printf("Infix : %s\n", infix);
+
+    infixPostfixeCevir(infix, postfix);
+
+    printf("Postfix : %s\n", postfix);
+    
+    printf("2017110206002 Bilal GULDEN");
+
+    return 0;
 }
